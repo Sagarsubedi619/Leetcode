8216d3fea705b89e992afc87a9bec688e94a7674
Sagar Subedi <sagarsubedi619@gmail.com>
2022-07-06 12:42:36 -0500

    new programs added

---
+++ src/Leetcode_main.java
@@ -0,0 +1,18 @@
+public class Leetcode_main {
+
+    public static void main(String[] args) {
+        int[] anArray={1,2,9,4,5,7};
+
+        pivotIndex demo2=new pivotIndex();
+        /*
+      RunningSum demo1=new RunningSum();
+        var x=demo1.solution(anArray);
+        for(int i:x){
+           System.out.println(i);
+        }*/
+        System.out.println(demo2.solution(anArray));
+
+    }
+
+
+}
